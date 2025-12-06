9006ea160e9043ffe09827b1f536c19022c74268
Khadraoui Hiba <hiba.khadraoui@esprit.tn>
2025-12-06 19:15:37 +0100

    Chap4 – Last Name

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM eclipse-temurin:11-jdk
+LABEL maintainer="Hiba Khadraoui - 4TWIN4"
+
+WORKDIR /app
+COPY target/*.jar app.jar
+EXPOSE 8082
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -71,8 +71,9 @@ public class User implements Serializable {
 	}
 
 	public String getLastName() {
-		return lastName;
-	}
+                return lastName + "-v1";
+        }
+
 	public void setLastName(String lName) {
 		this.lastName = lName;
 	}
